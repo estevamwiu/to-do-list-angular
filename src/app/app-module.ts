a313392c4657e2b6be012febc70342b69814d789
William Estevam <estevamwilliam16@gmail.com>
2025-10-20 16:25:30 -0300

    :sparkles: feat: Creation of the Home, Footer, NavBar components and styling of the Home screen.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HomeComponent } from './home-component/home-component';
+import { FooterComponent } from './footer-component/footer-component';
+import { NavBarComponent } from './nav-bar-component/nav-bar-component';
+import { MyTasksComponent } from './my-tasks-component/my-tasks-component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HomeComponent,
+    FooterComponent,
+    NavBarComponent,
+    MyTasksComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { MyTasksComponent } from './my-tasks-component/my-tasks-component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path: 'home', component: HomeComponent},
+  {path: 'my-tasks', component: MyTasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
